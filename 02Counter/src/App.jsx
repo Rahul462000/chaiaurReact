29d888ed23ddb82a058eaae4c2bf9272c32db087
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-10-17 11:47:11 +0530

    Counter application with conditions

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -8,27 +8,38 @@ function App() {
   // here we can use any nameconvention that we want
   // like
   // let [counter, chaiCounter] = useState(15);
-  let [counter, setCounter] = useState(15);
+  let [counter, setCounter] = useState(0);
 
   // let Counter = 15;
   const addValue = () => {
     // updating the counter
-    console.log("updating....", counter);
     // counter = counter + 1;
-    setCounter(counter + 1);
+    if (counter < 20) {
+      setCounter(counter + 1);
+    } else {
+      alert("counter limit cannot be more than 20");
+    }
+    console.log("updating....", counter);
   };
   const removeValue = () => {
-    setCounter(counter - 1);
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      alert("counter valur cannot be below 0");
+    }
   };
-
+  const resetValue = () => {
+    setCounter("");
+  };
   return (
     <>
       <h1>chai aur react</h1>
       <h2>Counter Value:{counter}</h2>
-      <button onClick={addValue}>ADD VAlue {counter}</button>
-      <br />
-      <button onClick={removeValue}>REMOVE VAlue</button>
-      <p>footer:{counter}</p>
+      <button onClick={addValue}>ADD VAlue :{counter}</button>
+      <hr />
+      <button onClick={removeValue}>REMOVE VAlue :{counter} </button>
+      <hr />
+      <button onClick={resetValue}>footer:{counter}</button>
     </>
   );
 }
