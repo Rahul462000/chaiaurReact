3e01ac73f3187948136278a5b7f30a8776d6d90a
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-10-18 08:26:42 +0530

    TAilwindCss Basics With Props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,33 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  // passing an object with props
+  let myObj = {
+    usename: "singh",
+    age: 21,
+  };
+  // passing an arrray with props
+  let newArray = [1, 2, 3];
 
   return (
     <>
       <h1 className="bg-green-400 text-black p-3 rounded-xl mb-4">
         Tailwind Test
       </h1>
-      <div class="relative h-[400px] w-[300px] rounded-md">
-        <img
-          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-          alt="AirMax Pro"
-          class="z-0 h-full w-full rounded-md object-cover"
-        />
-        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-        <div class="absolute bottom-4 left-4 text-left">
-          <h1 class="text-lg font-semibold text-white">Delba</h1>
-          <p class="mt-2 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            debitis?
-          </p>
-          <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-            View Profile →
-          </button>
-        </div>
-      </div>
+      {/* props tutorial from here */}
+      {/* <Card channel="chai aur code" someObj={newArray} /> */}
+      <Card username="chai aur code" btnText="CLick Me" />
+      <Card username="Tea with Code" btnText="Visit Me" />
+      {/* props ends here */}
     </>
   );
 }
